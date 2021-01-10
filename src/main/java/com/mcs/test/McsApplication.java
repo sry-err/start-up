1865522aab74f10659da7872f98c542b2505afb7
sry4err <sry4err@gmail.com>
2021-01-10 18:36:49 +0900

    二쇱꽍

---
+++ src/main/java/com/mcs/test/McsApplication.java
@@ -0,0 +1,13 @@
+package com.mcs.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mcs/test/McsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mcs.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
